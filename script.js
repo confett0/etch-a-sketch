1045f26a124a92d8b7f207809733ff3f47430e9d
confett0 <118047961+confett0@users.noreply.github.com>
2022-12-13 21:28:53 +0100

    Add resize grid functionality

--- script.js
+++ script.js
@@ -3,37 +3,74 @@
 const grid = document.querySelector('.container');
 const black = document.getElementById('black');
 const rainbow = document.getElementById('rainbow');
+let square;
+let squares;
 let blackMode = true;
 let rainbowMode;
-let numOfSquares = 60;
 
-black.addEventListener('click', function()
-{
+black.addEventListener('click', function () {
     rainbowMode = false;
     return blackMode = true;
-}
-)
+})
 
-rainbow.addEventListener('click', function()
-{   
+rainbow.addEventListener('click', function () {
     blackMode = false;
     return rainbowMode = true;
-}
-)
-
+})
 
 function createGrid(num) {
     for (let i = 1; i <= num ** 2; i++) {
-        let square = document.createElement('div');
+        square = document.createElement('div');
         square.className = 'square';
         grid.appendChild(square);
         grid.style.gridTemplateRows = `repeat(${num}, 1fr)`;
         grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-
     }
 }
 
-createGrid(numOfSquares);
+createGrid(32);
+
+function draw() {
+squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+    square.addEventListener("mouseover", function() {
+        if (rainbowMode) {
+        return square.style.backgroundColor = randomColor();
+    } else {
+        return square.style.backgroundColor = 'black';
+    }
+    })
+})}
+
+draw();
+
+function resetGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+let sizeButtons = document.querySelectorAll('.size');
+sizeButtons.forEach(sizeButton => {
+    sizeButton.addEventListener("click", function () {
+        if (sizeButton.classList.contains('small')) {
+            resetGrid()
+            //blackMode = true;
+            createGrid(64);
+            draw();
+        } else if (sizeButton.classList.contains('medium')) {
+            resetGrid();
+            //blackMode = true;
+            createGrid(32);
+            draw();
+        } else if (sizeButton.classList.contains('big')) {
+            resetGrid();
+            //blackMode = true;
+            createGrid(16);
+            draw();
+        }
+    })
+})
 
 function randomColor() {
     let color = "#";
@@ -42,23 +79,9 @@ function randomColor() {
 
 }
 
-let squareDivs = document.querySelectorAll('.square');
-squareDivs.forEach(box => {
-    box.addEventListener("mouseover", function() {
-        if (rainbowMode) {
-        return box.style.backgroundColor = randomColor();
-    } else {
-        return box.style.backgroundColor = 'black';
-    }
-    })
-})
-
-// Clear grid
+document.getElementById('clear').addEventListener('click', clearDiv);
 
 function clearDiv() {
-    squareDivs.forEach(box => {
-           return box.style.backgroundColor = "white";
-        })
-}
-
-document.getElementById('clear').addEventListener('click',clearDiv);
+    squares.forEach(square => {
+        return square.style.backgroundColor = "white";
+    })}
