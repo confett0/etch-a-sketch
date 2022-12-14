c79462c66b3574a2bca2d9c9f669a8ca56b8689c
confett0 <118047961+confett0@users.noreply.github.com>
2022-12-14 17:02:59 +0100

    Add eraser

--- script.js
+++ script.js
@@ -3,19 +3,29 @@
 const grid = document.querySelector('.container');
 const black = document.getElementById('black');
 const rainbow = document.getElementById('rainbow');
+const eraser = document.getElementById('eraser');
 let square;
 let squares;
 let blackMode = true;
 let rainbowMode;
+let eraserMode;
 
 black.addEventListener('click', function () {
     rainbowMode = false;
+    eraserMode = false;
     return blackMode = true;
 })
 
 rainbow.addEventListener('click', function () {
     blackMode = false;
+    eraserMode = false;
     return rainbowMode = true;
+})
+
+eraser.addEventListener('click', function () {
+    rainbowMode = false;
+    blackMode = false;
+    return eraserMode = true;
 })
 
 function createGrid(num) {
@@ -31,16 +41,20 @@ function createGrid(num) {
 createGrid(32);
 
 function draw() {
-squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-    square.addEventListener("mouseover", function() {
-        if (rainbowMode) {
-        return square.style.backgroundColor = randomColor();
-    } else {
-        return square.style.backgroundColor = 'black';
-    }
+    squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener("mouseover", function () {
+            if (rainbowMode) {
+                return square.style.backgroundColor = randomColor();
+            } else if (eraserMode)  {
+                return square.style.backgroundColor = 'white';
+            }
+            else {
+                return square.style.backgroundColor = 'black';
+            }
+        })
     })
-})}
+}
 
 draw();
 
@@ -48,24 +62,25 @@ function resetGrid() {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
+    if (eraserMode) {
+        eraserMode = false;
+        return blackMode = true;
+    }
 }
 
 let sizeButtons = document.querySelectorAll('.size');
 sizeButtons.forEach(sizeButton => {
     sizeButton.addEventListener("click", function () {
         if (sizeButton.classList.contains('small')) {
-            resetGrid()
-            //blackMode = true;
+            resetGrid();
             createGrid(64);
             draw();
         } else if (sizeButton.classList.contains('medium')) {
             resetGrid();
-            //blackMode = true;
             createGrid(32);
             draw();
         } else if (sizeButton.classList.contains('big')) {
             resetGrid();
-            //blackMode = true;
             createGrid(16);
             draw();
         }
@@ -76,7 +91,6 @@ function randomColor() {
     let color = "#";
     let hex = Math.floor(Math.random() * 16777215).toString(16);
     return color += hex;
-
 }
 
 document.getElementById('clear').addEventListener('click', clearDiv);
@@ -84,4 +98,5 @@ document.getElementById('clear').addEventListener('click', clearDiv);
 function clearDiv() {
     squares.forEach(square => {
         return square.style.backgroundColor = "white";
-    })}
+    })
+}
